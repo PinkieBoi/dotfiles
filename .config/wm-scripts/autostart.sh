229b817e99f3343f2f949693ce4df71419e49b56
James MacPherson <jtdmacpherson@pinkieboi.com>
2021-06-29 16:53:34 +0100

    window manager startup scripts

--- .config/wm-scripts/autostart.sh
+++ .config/wm-scripts/autostart.sh
@@ -16,9 +16,9 @@ numlockx on &
 xfce4-power-manager &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
-picom --config $HOME/.config/qtile/scripts/picom.conf &
+picom --config $HOME/.config/wm-scripts/picom.conf &
 nm-applet &
-variety &
+# variety &
 pamac-tray &
 blueman-applet &
 kdeconnect-indicator &
